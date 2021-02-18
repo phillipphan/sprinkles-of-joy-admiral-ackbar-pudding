44ee24d0ef61a08579f4b9e0b76d1cef3eaa0d2e
phillipphan <itsphillipphan@gmail.com>
2021-02-18 14:44:45 -0600

    menu dropdown shows up

--- scripts/products/ProductList.js
+++ scripts/products/ProductList.js
@@ -5,23 +5,24 @@ import { Product } from "./Product.js"
 const eventHub = document.querySelector("#container")
 const contentTarget = document.querySelector(".menu__items")
 
-let bakeryProducts = []
-let bakeryCategories = []
-
 export const ProductList = () => {
   getProducts()
     .then(getCategories)
     .then(() => {
-      bakeryProducts = useProducts()
-      bakeryCategories = useCategories()
-      render()
+      const bakeryProducts = useProducts()
+      const bakeryCategories = useCategories()
+      render(bakeryProducts, bakeryCategories)
     })
 }
 
-const render = () => {
+const render = (bakeryProducts, bakeryCategories) => {
   contentTarget.innerHTML = bakeryProducts.map(product => {
     const productCategory = bakeryCategories.find(cat => cat.id === product.categoryId)
 
     return Product(product, productCategory)
   }).join("")
 }
+
+eventHub.addEventListener("categorySelected", e => {
+  
+})
