9896cb2648b6ec966eabfff81a52b959d06868a9
phillipphan <itsphillipphan@gmail.com>
2021-02-22 14:49:22 -0600

    added the about section and some css for the modals

--- scripts/about/about.js
+++ scripts/about/about.js
@@ -7,9 +7,9 @@ eventHub.addEventListener("click", e => {
     contentTarget.innerHTML = `
       <div id="about__modal" class="modal--parent">
         <div id="about__content" class="modal--content">
-          <section>Welcome to the best way to purchase baked goods!! There has definitely never been an app like this!!! Create an account and become a rewards member for a good time!!!!</section>
+          <section>Welcome to the best way to purchase baked goods!! There has definitely never been an app like this!!! Create an account and become a rewards member for a good time!!!!</section><br>
           <section><a target="_blank" href="https://github.com/nss-day-cohort-46/sprinkles-of-joy-admiral-ackbar-pudding">GitHub</a></section>
-          <section>&copy; Phillip Phan and Caleb James</section>
+          <section>&copy; Phillip Phan and Caleb James</section><br>
           <button id="modal--close">Close</button>
           <div class="modal__message"></div>
         </div>
