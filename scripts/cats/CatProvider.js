7faf5ecb92a5d112058e6135c2dcddd5deb21416
Caleb James <calebsjames@gmail.com>
2021-02-23 13:15:31 -0600

    cat dropdown showing

--- scripts/cats/CatProvider.js
+++ scripts/cats/CatProvider.js
@@ -2,7 +2,7 @@
 let cats = [];
 
 // returns cats as objects
-export const useCats = () => cats.data.slice();
+export const useCats = () => cats
 
 
 //function to get cats from API
@@ -10,7 +10,7 @@ export const getCats = () => {
     debugger
     return fetch("https://api.thecatapi.com/v1/breeds?api_key=b7417677-f0a2-4fce-b5fe-cefd4bb44aa0")
     .then(response => response.json())
-    .then(catsArray => cats = catsArray.name)
+    .then(catsArray => cats = catsArray)
     .then(console.log(cats))
     
 }
